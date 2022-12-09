78f4ee5aa1b4940c1f5ed2a5fa74fc4ee6da0e74
jeff ndungu <ndungujeff.ke@gmail.com>
2022-12-09 21:34:10 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word.split("").reverse().join("");
+
 }
 
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
+}
 /* 
   Add your pseudocode here
 */
-
+//return word.split("").reverse().join("");
 /*
   Add written explanation of your solution here
 */
+
+// const word_array = word.split(""); 
+// using .split to remove blank spaces /spliting a string into an array of substrings
+// const reverse = wordArray.reverse(); // using .reverse to reverse the word
+// const reversed_word = reversedWordArray.join("");  //returning our array as a string using the join method
+// return reversed_word;
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
